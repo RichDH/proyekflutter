df1fa08f9bbeb0f86e34945ca6b38a547e1bd30b
RichDH <104489907+RichDH@users.noreply.github.com>
2025-06-06 19:49:35 +0700

    fix error register

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Stream<User?> get authStateChanges => _auth.authStateChanges();
   User? get currentUser => _auth.currentUser;
@@ -11,21 +13,26 @@ class AuthService {
       UserCredential result = await _auth.signInWithEmailAndPassword(email: email, password: password);
       return result.user;
     } catch (e) {
-      print(e.toString());
-      return null;
+      rethrow;
     }
   }
-
   Future<User?> registerWithEmail(String email, String password) async {
     try {
       UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
-      return result.user;
+      User? user = result.user;
+
+      if (user != null) {
+        await _firestore.collection('users').doc(user.uid).set({
+          'uid': user.uid,
+          'email': email,
+          'createdAt': Timestamp.now(),
+        });
+      }
+      return user;
     } catch (e) {
-      print(e.toString());
-      return null;
+      rethrow;
     }
   }
-
   Future<void> signOut() async {
     await _auth.signOut();
   }
